681f9da3268931fbdd00a764afb7752efe8b549d
elmesheryakova <e.meshcheryakova@dairyculture.ru>
2022-05-07 20:57:47 +0300

    feat: render sympols to page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log("Hello World!");
+import symbols from './symbols';
+import Keys from './Keys';
+
+const generateKeys = () => {
+  const keysArr = [];
+  symbols.forEach((el) => {
+    keysArr.push(new Keys(el));
+  });
+  return keysArr;
+};
+const renderKeys = () => {
+  const keyContainer = document.createElement('div');
+  keyContainer.classList.add('keyboard');
+  generateKeys().forEach((el) => {
+    keyContainer.append(String.fromCharCode(el.charcode));
+  });
+  document.body.append(keyContainer);
+};
+document.addEventListener('DOMContentLoaded', () => {
+  if (symbols) {
+    generateKeys();
+    renderKeys();
+  }
+});
