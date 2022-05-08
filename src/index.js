20f2991e24a06d48a85dfc75839e58b212b9b9ef
elmesheryakova <e.meshcheryakova@dairyculture.ru>
2022-05-08 14:54:25 +0300

    feat: change lang

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
 import './style.scss';
-import data from './symbols';
-import Keys from './Keys';
+import eng from './modules/symbols';
+import rus from './modules/ru-symbols';
+import Keys from './modules/Keys';
 
 const createTextarea = () => {
   const textArea = document.createElement('textarea');
   textArea.classList.add('textarea');
+  textArea.focus();
   document.body.prepend(textArea);
   return textArea;
 };
 
 const generateKeys = () => {
   const keysArr = [];
-  data.forEach((el) => {
-    keysArr.push(new Keys(el));
-  });
+  if (localStorage.getItem('lang') === 'ru') {
+    rus.forEach((el) => {
+      keysArr.push(new Keys(el));
+    });
+  } else {
+    eng.forEach((el) => {
+      keysArr.push(new Keys(el));
+    });
+  }
 
   return keysArr;
 };
@@ -26,27 +34,68 @@ const renderKeys = () => {
   });
   document.body.append(keyContainer);
 };
+
+const changeLang = (e) => {
+  if (e.code === 'ShiftLeft') {
+    document.onkeyup = (event) => {
+      if (event.code === 'AltLeft') {
+        if (localStorage.getItem('lang') === 'ru') {
+          localStorage.setItem('lang', 'en');
+          document.body.innerHTML = '';
+          createTextarea();
+          renderKeys();
+        } else if (localStorage.getItem('lang') === 'en') {
+          localStorage.setItem('lang', 'ru');
+          document.body.innerHTML = '';
+          createTextarea();
+          renderKeys();
+        }
+      }
+    };
+  }
+};
+
 document.addEventListener('DOMContentLoaded', () => {
-  if (data) {
+  if (eng) {
     renderKeys();
   }
   createTextarea();
 
   const keys = document.querySelectorAll('.key');
   document.addEventListener('keydown', (e) => {
-    keys.forEach((el) => {
-      el.classList.remove('active');
-    });
-    document.querySelector(`[data-key = ${e.key}]`).classList.add('active');
+    if (keys) {
+      keys.forEach((el) => {
+        el.classList.remove('active');
+      });
+    }
+    changeLang(e);
+
+    document.querySelector(`[data-key = ${e.code}]`).classList.add('active');
   });
+
   keys.forEach((el) => {
     el.addEventListener('click', (e) => {
       if (e !== null && e.target instanceof HTMLElement) {
+        let value = e.target.textContent.trim();
+        const field = document.querySelector('.textarea');
         keys.forEach((elem) => {
           elem.classList.remove('active');
         });
         document.querySelector(`[data-key = ${e.target.dataset.key}]`).classList.add('active');
-        document.querySelector('.textarea').innerHTML += e.target.dataset.key;
+        if (value === 'Backspace') value = '';
+        if (value === 'Tab') value = '';
+        if (value === 'Delete') value = '';
+        if (value === 'CapsLock') value = '';
+        if (value === 'Enter') value = '';
+        if (value === 'Shift') value = '';
+        if (value === 'Ctrl') value = '';
+        if (value === 'WIN') value = '';
+        if (value === 'Alt') value = '';
+        if (value === '↑') value = '';
+        if (value === '←') value = '';
+        if (value === '↓') value = '';
+        if (value === '→') value = '';
+        field.innerHTML += value;
       }
     });
   });
--- src/modules/Keys.js
+++ src/modules/Keys.js
@@ -1,12 +1,14 @@
 export default class Keys {
-  constructor({ key }) {
+  constructor({ key, code, valueShift }) {
     this.key = key;
+    this.code = code;
+    this.valueShift = valueShift;
   }
 
   createdKeys() {
     const key = document.createElement('div');
     key.classList.add('key');
-    key.setAttribute('data-key', this.key);
+    key.setAttribute('data-key', this.code);
     key.innerHTML = `
     ${this.key}
     `;
