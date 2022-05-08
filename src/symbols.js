22da87740a7837857a4156355904eb2c0f640e82
elmesheryakova <e.meshcheryakova@dairyculture.ru>
2022-05-09 00:26:50 +0300

    fix conflict

---
+++ src/symbols.js
@@ -0,0 +1,11 @@
+const symbols = [
+  { charcode: 113 }, { charcode: 119 }, { charcode: 101 }, { charcode: 114 },
+  { charcode: 116 }, { charcode: 121 }, { charcode: 117 }, { charcode: 105 },
+  { charcode: 111 }, { charcode: 112 }, { charcode: 91 }, { charcode: 93 },
+  { charcode: 92 }, { charcode: 97 }, { charcode: 115 }, { charcode: 100 }, { charcode: 102 },
+  { charcode: 103 }, { charcode: 104 }, { charcode: 106 }, { charcode: 107 }, { charcode: 108 },
+  { charcode: 59 }, { charcode: 39 }, { charcode: 122 }, { charcode: 120 }, { charcode: 99 },
+  { charcode: 118 }, { charcode: 98 }, { charcode: 110 }, { charcode: 109 }, { charcode: 44 },
+  { charcode: 46 }, { charcode: 47 },
+];
+export default symbols;
